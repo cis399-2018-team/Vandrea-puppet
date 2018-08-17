c37e37534ee76ce1e8777f05c0541b9b207fbd23
Vu Vo <vuv@uoregon.edu>
2018-08-16 17:39:07 -0700

    nodejs, tmux modules

--- manifests/site.pp
+++ manifests/site.pp
@@ -8,12 +8,28 @@ node ip-10-0-4-109 {
 
 node ip-10-0-4-211 {
 	include sshd
-  include apache
-  include users
+  	include apache
+  	include users
+	class { 'nodejs': }
+        class { '::nodejs':
+        nodejs_dev_package_ensure => 'present',
+        npm_package_ensure        => 'present',
+        repo_class                => '::epel',
+        }
+        class { '::tmux': }
+
 }
 
 node ip-10-0-4-48 {
 	include sshd
-  include apache
-  include users
+  	include apache
+  	include users
+	class { 'nodejs': }
+        class { '::nodejs':
+        nodejs_dev_package_ensure => 'present',
+        npm_package_ensure        => 'present',
+        repo_class                => '::epel',
+        }
+        class { '::tmux': }
+
 }
